43891ee425a79503aa79becea99e42692beb3372
Riyasfrd <riyasf@alliontechnologies.com>
2024-08-28 10:24:30 +0530

    modules change

--- src/modules/raw-materials/raw-materials.controller.ts
+++ src/modules/raw-materials/raw-materials.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { RawMaterialsService } from './raw-materials.service';
 import { RawMaterial } from './schemas/raw-material.schema';
