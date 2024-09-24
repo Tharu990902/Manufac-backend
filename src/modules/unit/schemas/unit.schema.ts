ecc0b05b6eaec113f99e8c774cae6937ea5b8d31
Tharuallion <tharushik@alliontechnologies.com>
2024-09-24 20:34:23 +0530

    changes_unit

---
+++ src/modules/unit/schemas/unit.schema.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type UnitDocument = Unit & Document;
+
+@Schema()
+export class Unit {
+  @Prop({ required: true })
+      unitOfMeasure: string;
+}
+
+export const UnitSchema = SchemaFactory.createForClass(Unit);
