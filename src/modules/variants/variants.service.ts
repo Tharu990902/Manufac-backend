ea56c6c96aa03998f4fddb180d8cbd4161285391
chamidilshanallion <chamikas@alliontechnologies.com>
2024-09-24 12:26:45 +0530

    added variant value short name availabilty and generating short name

--- src/modules/variants/variants.service.ts
+++ src/modules/variants/variants.service.ts
@@ -16,6 +16,45 @@ export class VariantsService {
         const createdVariant = new this.variantModel(createdVariantDto);
         return createdVariant.save();
       }
+
+      async isShortNameAvailable(shortName: string): Promise<boolean> {
+        const variants = await this.variantModel.find({
+          'values.shortName': shortName,
+        }).exec();
+    
+        return variants.length === 0;
+      }
+
+      async generateUniqueShortName(value: string): Promise<string> {
+        const words = value.split(' ').filter(word => word.length > 0);
+        
+        let shortName;
+
+        if (words.length === 1) {
+            shortName = words[0].substring(0, 3).toUpperCase(); // Take the first 3 letters
+        } else {
+            shortName = words
+              .map(word => word[0].toUpperCase()) 
+              .join('');
+        }
+      
+        let isShortNameUnique = await this.isShortNameAvailable(shortName);
+      
+        // If the generated short name is not unique, append a number or change logic
+        if (!isShortNameUnique) {
+          for (let i = 1; i < 1000; i++) {
+            const newShortName = `${shortName}${i}`;
+            const isAvailable = await this.isShortNameAvailable(newShortName);
+            if (isAvailable) {
+              return newShortName;
+            } 
+          }
+          throw new Error('Could not generate a unique short name');
+        }
+      
+        return shortName;
+      }
+      
       
     
       async findAll(): Promise<Variant[]> {
