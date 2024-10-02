b0355da0a93d2bbc1a95f89addd4415196d9b6cb
sajad-allion <sajadd@alliontechnologies.com>
2024-10-02 09:43:05 +0530

    supplier

--- src/modules/supplier/schemas/supplier.schema.ts
+++ src/modules/supplier/schemas/supplier.schema.ts
@@ -1,26 +1,27 @@
 /* eslint-disable prettier/prettier */
-import { Schema, SchemaFactory } from "@nestjs/mongoose";
+// /* eslint-disable prettier/prettier */
+// import { Schema, SchemaFactory } from "@nestjs/mongoose";
 
-@Schema()
-export class SupplierRegistration extends Document {
+// @Schema()
+// export class SupplierRegistration extends Document {
 
-    @Prop({required: true})
-    supplierName: string;
+//     @Prop({required: true})
+//     supplierName: string;
 
-    @Prop({required: true, unique: true})
-    supplierCode: string;
+//     @Prop({required: true, unique: true})
+//     supplierCode: string;
     
-    @Prop({required: true})
-    description: string;
+//     @Prop({required: true})
+//     description: string;
 
-    @Prop({required: true})
-    contactNumber: number;
+//     @Prop({required: true})
+//     contactNumber: number;
 
-    @Prop({required: true})
-    email: string;
+//     @Prop({required: true})
+//     email: string;
 
-    @Prop({required: true})
-    address: string;
-}
+//     @Prop({required: true})
+//     address: string;
+// }
 
-export const SupplierRegistrationSchema = SchemaFactory.createForClass(SupplierRegistration);
+// export const SupplierRegistrationSchema = SchemaFactory.createForClass(SupplierRegistration);
